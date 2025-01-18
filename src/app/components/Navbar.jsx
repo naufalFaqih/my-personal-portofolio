0391d74af0876d4a3951b85f89a122049ead257f
naufalFaqih <naufalfaqih460@gmail.com>
2025-01-18 15:31:15 +0700

    add About component and enhance Navbar with scroll effect

--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -1,8 +1,9 @@
-import React, { useRef } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import Image from 'next/image';
 import { assets } from '../../../assets/assets';
 
 const Navbar = () => {
+  const [isScroll, setIsScroll] = useState(false);
   const sideMenuRef = useRef();
   const openMenu = () => {
     sideMenuRef.current.style.transform = 'translateX(-16rem)';
@@ -11,16 +12,25 @@ const Navbar = () => {
   const closeMenu = () => {
     sideMenuRef.current.style.transform = 'translateX(16rem)';
   };
+  useEffect(() => {
+    window.addEventListener('scroll', () => {
+      if (scrollY > 50) {
+        setIsScroll(true);
+      } else {
+        setIsScroll(false);
+      }
+    });
+  }, []);
   return (
     <>
       <div className="fixed top-0 right-0 w-11/12 -z-10 translate-y-[-80%]">
         <Image src={assets.header_bg_color} alt="bg-color" className="w-full" />
       </div>
-      <nav className="w-full fixed px-5 lg:px-8 xl:px-[8%] py-4 flex items-center justify-between z-50">
+      <nav className={`w-full fixed px-5 lg:px-8 xl:px-[8%] py-4 flex items-center justify-between z-50 ${isScroll ? 'bg-white bg-opacity-50 backdrop-blur-lg shadow-sm' : ''}`}>
         <a href="#top">
           <Image src={assets.logo_black} className='w-28 cursor-pointer mr-14 alt="logo_black"' />
         </a>
-        <ul className="hidden md:flex items-center gap-6 lg:gap-8 rounded-full px-12 py-3 bg-white shadow-sm bg-opacity-50">
+        <ul className={`hidden md:flex items-center gap-6 lg:gap-8 rounded-full px-12 py-3 ${isScroll ? '' : 'bg-white shadow-sm bg-opacity-50'}`}>
           <li>
             <a href="#top" className="font-Ovo">
               Home
