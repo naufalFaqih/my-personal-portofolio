ed639653a687046d49ddf57abfd849e5269ec3f9
naufalFaqih <naufalfaqih460@gmail.com>
2026-06-04 18:20:24 +0700

    Update configuration and modify Work component link for improved navigation

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  basePath: '/my-personal-portofolio',
+};
 
 export default nextConfig;
